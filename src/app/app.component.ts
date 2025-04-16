82216e71914bcff64b4b8689e5efa5b7d2bfa015
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2025-04-16 16:18:32 +0900

    readme update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-debug';
+
+  ngOnInit() {
+    console.log("test");
+    console.log("test1");
+    console.log("test2");
+    console.log("test3");
+    console.log("test4");
+    console.log("test5");
+    console.log("test6");
+    console.log("test7");
+    console.log("test8");
+    console.log("test9");
+  }
 }
